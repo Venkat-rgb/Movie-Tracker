661ff7928295ae7def3fdf94693d3ac009beb65b
Venkat <venkymandadi143@gmail.com>
2022-09-08 14:48:35 +0530

    Movies App Tracker

---
+++ js/movie.js
@@ -0,0 +1,260 @@
+"use strict";
+
+const movieDetails = localStorage.getItem("movie_link");
+const rightContent = document.querySelector(".right__content");
+const movieContent = document.querySelector(".movie__content");
+// const actors = document.querySelector(".actors");
+const movieCast = document.querySelector(".movie__cast .swiper-wrapper");
+const movieCrew = document.querySelector(".movie__crew .swiper-wrapper");
+
+const movieTrailer = document.querySelector(".movie__trailer");
+const movieHeading = document.querySelector(".movie__heading");
+const movieCastHeading = document.querySelector(".movie__cast--heading");
+const movieCrewHeading = document.querySelector(".movie__crew--heading");
+const movieTrailerHeading = document.querySelector(".movie__trailer--heading");
+const leftBtn = document.querySelector(".left__btn");
+const rightBtn = document.querySelector(".right__btn");
+
+const API_KEY = "04c35731a5ee918f014970082a0088b1";
+
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
+
+const IMG_URL = `https://image.tmdb.org/t/p/w500`;
+
+const blankImage = `https://www.diabetes.ie/wp-content/uploads/2017/02/no-image-available-250x417.png`;
+
+setTimeout(() => {
+  movieHeading.classList.remove("hide");
+  movieCastHeading.classList.remove("hide");
+  movieCrewHeading.classList.remove("hide");
+  movieTrailerHeading.classList.remove("hide");
+}, 1250);
+
+function getDuration(min) {
+  const hours = min / 60;
+  const afterDecimal = Math.floor(min / 60);
+  const minutes = Math.round((hours - afterDecimal) * 60);
+  return `${afterDecimal}hr ${minutes}min`;
+}
+
+function setMovieInfo(imageData, movieLanguages, movieGenres) {
+  const htmlData1 = `
+        <p class="movie__name">Name :<span>${imageData.title}</span></p>
+        <p class="movie__tagline">Tag Line :<span>--   ${
+          imageData.tagline
+        } </span></p>
+    
+        <p class="movie__language">Language :<span>${movieLanguages.join(
+          ", "
+        )}</span></p>
+        <p class="movie__release__date">
+            Release Date :<span>${imageData.release_date}</span>
+        </p>
+        <p class="movie__genre">
+            Movie Genres :<span>${movieGenres.join(", ")}</span>
+        </p>
+        <p class="movie__story">
+            Story :<span
+            >${imageData.overview}</span>
+        </p>
+        <p class="movie__runtime">Duration :<span>${getDuration(
+          imageData.runtime
+        )}</span></p>
+        <p class="movie__budget">Movie Budget :<span>$${imageData.budget.toLocaleString(
+          "en-US"
+        )}</span></p>
+        <p class="movie__revenue">Movie Revenue :<span>$${imageData.revenue.toLocaleString(
+          "en-US"
+        )}</span></p>
+        <p class="movie__rating">IMDb Rating :<span>${
+          imageData.vote_average
+        }</span></p>
+    `;
+  rightContent.innerHTML = htmlData1;
+}
+
+function setCastAndCrew(actor, team = true) {
+  let img = `<img src=${IMG_URL}${actor.profile_path}>`;
+  if (!actor.profile_path) {
+    img = `<img src=${blankImage} class="no__image">`;
+  }
+
+  const htmlData = `
+            <div class="swiper-slide">
+                ${img}
+                <p class="name__in__real">${actor.original_name}</p>
+                <p class="name__in__movie">${
+                  team === true ? actor.character : actor.job
+                }</p>
+            </div>
+           `;
+  if (team === true) {
+    movieCast.insertAdjacentHTML("beforeend", htmlData);
+  } else {
+    movieCrew.insertAdjacentHTML("beforeend", htmlData);
+  }
+}
+
+async function setData() {
+  try {
+    const res = await fetch(movieDetails);
+
+    if (!res.ok) throw new Error("No Results Found");
+
+    const data = await res.json();
+    const [movieData] = data.results;
+    const movieId = movieData.id;
+
+    const imageRes = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`
+    );
+    if (!imageRes.ok) throw new Error("No Results Found");
+
+    const imageData = await imageRes.json();
+    // console.log(imageData);
+
+    const castLink = fetch(
+      `https://api.themoviedb.org/3/movie/${imageData.id}/credits?api_key=${API_KEY}&language=en-US`
+    );
+
+    const trailerLink = fetch(
+      `https://api.themoviedb.org/3/movie/${imageData.id}/videos?api_key=${API_KEY}&language=en-US`
+    );
+    const castAndTrailerLinks = await Promise.all([castLink, trailerLink]);
+
+    if (!castAndTrailerLinks[0].ok) throw new Error("No Results Found");
+    if (!castAndTrailerLinks[1].ok) throw new Error("No Results Found");
+
+    const castData = await castAndTrailerLinks[0].json();
+    const { results: trailerData } = await castAndTrailerLinks[1].json();
+    // console.log(trailerData);
+
+    const movieGenres = imageData.genres.map((genre) => {
+      return genre.name;
+    });
+
+    const movieLanguages = imageData.spoken_languages.map((language) => {
+      return language.english_name;
+    });
+
+    // setting cast data.
+    castData.cast.forEach((actor) => {
+      setCastAndCrew(actor);
+    });
+
+    // prettier-ignore
+    const crewCategories = ["Editor","Director","Producer","Original Music Composer","Writer"];
+
+    const crewArr = castData.crew.filter((item) => {
+      return crewCategories.includes(item.job);
+    });
+
+    crewArr.forEach((actor) => {
+      setCastAndCrew(actor, false);
+    });
+
+    // setting crew data.
+
+    let swiperPrev = document.querySelectorAll(".swiper-button-prev");
+    let swiperNext = document.querySelectorAll(".swiper-button-next");
+
+    function addDisplay(display) {
+      swiperPrev.forEach((item) => {
+        display === true
+          ? item.classList.add("hide")
+          : item.classList.remove("hide");
+      });
+
+      swiperNext.forEach((item) => {
+        display === true
+          ? item.classList.add("hide")
+          : item.classList.remove("hide");
+      });
+    }
+
+    function checkArrow() {
+      if (window.innerWidth <= 600) {
+        addDisplay(true);
+      } else {
+        addDisplay(false);
+      }
+    }
+
+    const swiper = new Swiper(".swiper", {
+      slidesPerView: 5,
+      spaceBetween: 30,
+      speed: 400,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        300: {
+          slidesPerView: 1,
+        },
+
+        350: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+
+        600: {
+          slidesPerView: 3,
+        },
+
+        800: {
+          slidesPerView: 4,
+        },
+
+        1100: {
+          slidesPerView: 5,
+        },
+      },
+      on: {
+        init: function () {
+          checkArrow();
+        },
+        resize: function () {
+          checkArrow();
+        },
+      },
+    });
+
+    // setting main movie image.
+    const htmlData2 = `<img class="movie__image" src=${IMG_URL}${imageData.poster_path}>`;
+    movieContent.innerHTML = htmlData2;
+
+    // setting movie info.
+    setMovieInfo(imageData, movieLanguages, movieGenres);
+
+    console.log(trailerData);
+
+    const officialTrailer = trailerData.find((item) => {
+      return (
+        item.name.includes("Official Trailer") ||
+        item.name.includes("Trailer") ||
+        item.name.includes("Official") ||
+        item.name.includes("Teaser")
+      );
+    });
+    // console.log(trailerData);
+    // setting trailer.
+    console.log(imageData);
+    const htmlData3 = `
+          <iframe
+            width="853"
+            height="550"
+            src="https://www.youtube.com/embed/${officialTrailer.key}"
+            title=""
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"    
+            allowfullscreen
+          >
+          </iframe>
+        `;
+    movieTrailer.insertAdjacentHTML("beforeend", htmlData3);
+  } catch (err) {
+    console.log(err);
+  }
+}
+setData();
---
+++ js/script.js
@@ -0,0 +1,232 @@
+"use strict";
+
+// ALL API LINKS.
+
+const API_KEY = "04c35731a5ee918f014970082a0088b1";
+
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
+
+const IMG_URL = `https://image.tmdb.org/t/p/w500`;
+
+const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?&api_key=${API_KEY}&query=`;
+
+const GET_DETAILS = `https://api.themoviedb.org/3/movie/473234?api_key=${API_KEY}&language=en-US`;
+
+const VIDEO_DETAILS = `https://api.themoviedb.org/3/movie/473234/videos?api_key=${API_KEY}&language=en-US`;
+
+const UPCOMING_DETAILS = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=2`;
+
+const CAST_DETAILS = `https://api.themoviedb.org/3/movie/690957/credits?api_key=${API_KEY}&language=en-US`;
+
+const GENRE_DETAILS = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`;
+
+// ALL VARIABLE DECLARATIONS.
+const cards = document.querySelector(".cards");
+const textBox = document.querySelector(".text-box");
+const movieDetails = document.querySelector(".movie__details");
+const overlay = document.querySelector(".overlay");
+const inputFieldDiv = document.querySelector(".input-field");
+const genres = document.querySelector(".genres");
+const prevBtn = document.querySelector(".prev__btn");
+const nextBtn = document.querySelector(".next__btn");
+const currBtn = document.querySelector(".current__btn");
+const pagination = document.querySelector(".pagination");
+const pTag = document.createElement("p");
+
+// changing colors for imdb rating.
+function changeColor(rating) {
+  if (rating >= 8) return "green";
+  else if (rating >= 5) return "orange";
+  else return "red";
+}
+
+function renderError(message) {
+  pTag.className = "error__message";
+  pTag.innerText = message;
+  pTag.classList.add("active");
+  inputFieldDiv.appendChild(pTag);
+}
+
+let currentPage = 1,
+  prevPage = 2,
+  nextPage = 3,
+  totalPages = 4,
+  lastUrl = "";
+
+// getting movie data.
+async function getData(url = API_URL) {
+  try {
+    cards.innerHTML = "";
+    textBox.value = "";
+
+    const res = await fetch(url);
+    if (!res.ok) throw new Error("No Results Found!");
+
+    const data = await res.json();
+    let currMovie = data.results;
+    if (currMovie.length === 0) {
+      genres.style.display = "none";
+      pagination.style.display = "none";
+      renderError("No Results Found!");
+    } else {
+      genres.style.display = "flex";
+      pagination.style.display = "flex";
+      if (pTag.classList.contains("active")) {
+        inputFieldDiv.removeChild(document.querySelector(".error__message"));
+        pTag.classList.remove("active");
+      }
+    }
+
+    lastUrl = url;
+    currentPage = data.page;
+    prevPage = currentPage - 1;
+    nextPage = currentPage + 1;
+    totalPages = data.total_pages;
+
+    currBtn.innerText = currentPage;
+
+    // console.log(data);
+
+    // if (currMovie.length === 0) {
+    //   throw new Error("No Results Found!");
+    // }
+
+    currMovie.forEach((movie) => {
+      const movieImg = movie.poster_path;
+      if (!movieImg) return;
+
+      const htmlData = `
+      <a href= "html/movie-details.html" class="card__link">
+          <div class="card__item">
+              <img class="card__img" src=${IMG_URL + movie.poster_path}>
+              <div class="card__details">
+                  <p class="card__details__name">${movie.title}</p>
+                  <span class="card__details__rating ${changeColor(
+                    movie.vote_average
+                  )}">${movie.vote_average}</span>
+              </div>
+          </div>
+          </a>
+      `;
+      cards.insertAdjacentHTML("beforeend", htmlData);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+getData();
+
+// when we change input text filed.
+textBox.addEventListener("change", function (e) {
+  if (this.value === "") return;
+  const giveHiphen = this.value.trim().split(" ").join("-");
+  const search_url = SEARCH_URL + giveHiphen;
+  getData(search_url);
+});
+
+let selectedGenre = [];
+
+function changePage(page) {
+  const urlSplit = lastUrl.split("?");
+  const splitParams = urlSplit[1].split("&");
+  const pageParam = splitParams[splitParams.length - 1].split("=");
+
+  if (pageParam[0] !== "page") {
+    let url = `${API_URL}&page=${page}`;
+    getData(url);
+  } else {
+    pageParam[1] = page.toString();
+    const joinPageParam = pageParam.join("=");
+    splitParams[splitParams.length - 1] = joinPageParam;
+    const joinSplitParam = splitParams.join("&");
+    const joinTotalUrl = `${urlSplit[0]}?${joinSplitParam}`;
+    getData(joinTotalUrl);
+  }
+}
+
+prevBtn.addEventListener("click", function () {
+  if (prevPage > 0) {
+    changePage(prevPage);
+  }
+});
+
+nextBtn.addEventListener("click", function () {
+  if (nextPage <= totalPages) {
+    inputFieldDiv.scrollIntoView({ behavior: "smooth" });
+
+    changePage(nextPage);
+  }
+});
+
+// displaying all types of genres.
+function clearGenre() {
+  const isBtnExist = document.querySelector(".genre__title--clear");
+  if (!isBtnExist) {
+    const clearBtn = document.createElement("button");
+    clearBtn.className = "genre__title--clear";
+    clearBtn.innerText = "Clear";
+    genres.appendChild(clearBtn);
+    clearBtn.addEventListener("click", function () {
+      selectedGenre = [];
+      setGenre();
+      getData();
+    });
+  }
+}
+
+async function setGenre() {
+  try {
+    genres.innerHTML = "";
+    const res = await fetch(GENRE_DETAILS);
+    const data = await res.json();
+    const allGenres = data.genres;
+
+    // console.log(allGenres);
+
+    allGenres.forEach((genre, i) => {
+      const currGenre = document.createElement("button");
+      currGenre.className = "genre__title";
+      currGenre.id = genre.id;
+      currGenre.innerText = genre.name;
+      genres.appendChild(currGenre);
+
+      currGenre.addEventListener("click", function () {
+        currGenre.style.backgroundColor = "rgb(255, 103, 103)";
+        if (selectedGenre.length === 0) {
+          selectedGenre.push(genre.id);
+        } else {
+          if (selectedGenre.includes(genre.id)) {
+            const index = selectedGenre.indexOf(genre.id);
+            selectedGenre.splice(index, 1);
+            currGenre.style.backgroundColor = "rgb(250, 177, 42)";
+          } else {
+            selectedGenre.push(genre.id);
+          }
+        }
+        getData(`${API_URL}&with_genres=${selectedGenre.join(",")}`);
+        clearGenre();
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+setGenre();
+
+async function getDataOnClick(e) {
+  try {
+    let currDetails = e.target.closest(".card__item");
+    if (!currDetails) return;
+
+    currDetails = currDetails.querySelector(".card__details__name");
+    const movieName = currDetails.innerText.split(" ").join("-");
+    const movieUrl = SEARCH_URL + movieName;
+
+    localStorage.setItem("movie_link", movieUrl);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// when we click on card_items this should happen.
+cards.addEventListener("click", getDataOnClick);
